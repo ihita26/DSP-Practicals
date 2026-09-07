af646565517c077f344e785451984fcb2fa914e3
ihita26 <meshramihita@gmail.com>
2026-09-07 08:36:37 -0700

    Add basic arithmetic and logical operations in p2.py

---
+++ p2.py
@@ -0,0 +1,21 @@
+a = 10
+b = 5
+#Arithmetic 
+print("Addition:", a + b)
+print("division:", a / b)
+#Relational
+print("a>b:", a > b)
+#Logical
+print("a>5 and b<10:", a > 5 and b < 10)
+#bitwise
+print("a & b:", a & b)
+#assignment
+a += b
+print("Updated a:", a)
+#Membership
+List1 = [1, 2, 3, 4]
+print("2 in list1:", 2 in List1)
+#Identity
+x = 10
+y = 10
+print("x is y:", x is y)
